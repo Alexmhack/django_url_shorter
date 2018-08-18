c115e275dde0edfbbe3b9644f092ea5967874ac9
alexHooper <alexmohack@gmail.com>
2018-08-18 17:42:56 +0530

    change Char fields to URL fields for url validation

--- shortener/models.py
+++ shortener/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class KirrURL(models.Model):
-	url = models.CharField(max_length=220)
-	shortcode = models.CharField(max_length=15, unique=True)
+	url = models.URLField(max_length=220)
+	shortcode = models.URLField(max_length=15, unique=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
