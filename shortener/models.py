e7850985fc22b98912c8dee479dc992c4024adcd
alexHooper <alexmohack@gmail.com>
2018-08-18 13:16:24 +0530

    define field for our short url which is max 15 chars long

--- shortener/models.py
+++ shortener/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class KirrURL(models.Model):
 	url = models.CharField(max_length=220)
+	shortcode = models.CharField(max_length=15)
 
 	def __str__(self):
 		return str(self.url)
