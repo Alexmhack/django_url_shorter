9e2e1400e04df4e8a3114fac915dc827cfcb5ffd
alexHooper <alexmohack@gmail.com>
2018-08-18 13:24:13 +0530

    add unique attribute to short urls

--- shortener/models.py
+++ shortener/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class KirrURL(models.Model):
 	url = models.CharField(max_length=220)
-	shortcode = models.CharField(max_length=15)
+	shortcode = models.CharField(max_length=15, unique=True)
 
 	def __str__(self):
 		return str(self.url)
