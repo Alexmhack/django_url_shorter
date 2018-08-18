75d9f72d5ee2c9bcd16c7c461dccce74c9b1ab4a
alexHooper <alexmohack@gmail.com>
2018-08-19 00:08:37 +0530

    add field active for url with default True

--- shortener/models.py
+++ shortener/models.py
@@ -7,6 +7,7 @@ class KirrURL(models.Model):
 	shortcode = models.URLField(max_length=15, unique=True, blank=True)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+	active = models.BooleanField(default=True)
 
 	def __str__(self):
 		return str(self.url)
