cacbc044e28ef10f00a8da27956da8e733dc6d5a
alexHooper <alexmohack@gmail.com>
2018-08-19 10:32:08 +0530

    creating func and class based sample views

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+def kirr_redirect_view(request, *args, **kwargs):
+	HttpResponse('<h1>Kirr Url Shortener</h1>')
+
+
+class KirrRedirectView(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("<h1>Kirr Url Shortener Again</h1>")
