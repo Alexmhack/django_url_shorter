84be56249f2dc2fa9c744b4013307916b09e1d96
alexHooper <alexmohack@gmail.com>
2018-08-19 21:02:55 +0530

    showing 404 page if query not found

--- shortener/views.py
+++ shortener/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 
 from .models import KirrURL
 
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs):
-	object = KirrURL.objects.get(shortcode=shortcode)
-	return HttpResponse(f'<h1>Kirr Url Shortener {object.url}</h1>')
+	object = get_object_or_404(KirrURL, shortcode=shortcode)
+	obj_url = object.url
+	return HttpResponse(f'<h1>Kirr Url Shortener {obj_url}</h1>')
 
 
 class KirrRedirectView(View):
 	def get(self, request, shortcode=None, *args, **kwargs):
-		object = KirrURL.objects.get(shortcode=shortcode)
-		return HttpResponse(f"<h1>Kirr Url Shortener Again {object.url}</h1>")
+		object = get_object_or_404(KirrURL, shortcode=shortcode)
+		obj_url = object.url
+		return HttpResponse(f"<h1>Kirr Url Shortener Again {obj_url}</h1>")
