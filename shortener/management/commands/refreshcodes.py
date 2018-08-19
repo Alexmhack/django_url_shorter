0c74658dda265f2fff1a385c72ba27eab5d94ec6
alexHooper <alexmohack@gmail.com>
2018-08-19 09:28:17 +0530

    add number argument in refreshcodes command

--- shortener/management/commands/refreshcodes.py
+++ shortener/management/commands/refreshcodes.py
@@ -5,5 +5,9 @@ from shortener.models import KirrURL
 class Command(BaseCommand):
     help = 'Resfreshes all KirrURL shortcodes'
 
+    def add_arguments(self, parser):
+        parser.add_argument('number', type=int)
+
     def handle(self, *args, **options):
         return KirrURL.objects.refresh_shortcodes()
+        
