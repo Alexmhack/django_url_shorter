f3dff0b1295cd1238cd7049d6b13a427a833f231
alexHooper <alexmohack@gmail.com>
2018-08-19 22:42:13 +0530

    redirect to the stored url in obj

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 
 from .models import KirrURL
@@ -7,11 +7,11 @@ from .models import KirrURL
 def kirr_redirect_view(request, shortcode=None, *args, **kwargs):
 	object = get_object_or_404(KirrURL, shortcode=shortcode)
 	obj_url = object.url
-	return HttpResponse(f'<h1>Kirr Url Shortener {obj_url}</h1>')
+	return HttpResponseRedirect(obj_url)
 
 
 class KirrRedirectView(View):
 	def get(self, request, shortcode=None, *args, **kwargs):
 		object = get_object_or_404(KirrURL, shortcode=shortcode)
 		obj_url = object.url
-		return HttpResponse(f"<h1>Kirr Url Shortener Again {obj_url}</h1>")
+		return HttpResponseRedirect(obj_url)
