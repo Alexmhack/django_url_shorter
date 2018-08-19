83bac03c53617beb021e0cff01e1e9d5c57c29ad
alexHooper <alexmohack@gmail.com>
2018-08-19 11:20:36 +0530

    use min shortcode var from project settings

--- shortener/utils.py
+++ shortener/utils.py
@@ -1,11 +1,15 @@
 import random
 import string
 
-def code_generator(size=5, chars=string.ascii_lowercase + string.digits + string.ascii_uppercase):
+from django.conf import settings
+
+SHORTCODE_MIN = getattr(settings, "SHORTCODE_MIN", 5)
+
+def code_generator(size=SHORTCODE_MIN, chars=string.ascii_lowercase + string.digits + string.ascii_uppercase):
 	return ''.join(random.choice(chars) for _ in range(size))
 
 
-def create_shortcode(instance, size=5):
+def create_shortcode(instance, size=SHORTCODE_MIN):
 	new_code = code_generator(size=size)
 	Klass = instance.__class__
 	qs_exists = Klass.objects.filter(shortcode=new_code).exists()
