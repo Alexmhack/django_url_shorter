3ae14eb0b5d3bc6eb4b4aef290907c8b57eb7654
alexHooper <alexmohack@gmail.com>
2018-08-20 16:58:38 +0530

    use validator func in the url field

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+def validate_url(value):
+	url_validator = URLValidator()
+	try:
+		url_validator(value)
+	except Exception as e:
+		raise ValidationError(e)
+	return value
+
 
 class ShortenURLForm(forms.Form):
-	url = forms.URLField()
+	url = forms.URLField(validators=[validate_url])
