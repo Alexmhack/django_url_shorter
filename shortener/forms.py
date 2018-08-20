d146f67dba5d9450263527f5bffa7e4eeccbd957
alexHooper <alexmohack@gmail.com>
2018-08-20 16:45:21 +0530

    remove validator funcs from the form

--- shortener/forms.py
+++ shortener/forms.py
@@ -3,16 +3,3 @@ from django.core.validators import URLValidator
 
 class ShortenURLForm(forms.Form):
 	url = forms.URLField()
-
-	def clean(self):
-		cleaned_data = super().clean()
-
-	def cleaned_url(self):
-		url = self.cleaned_data['url']
-		print(url)
-		url_validator = URLValidator()
-		try:
-			url_validator(url)
-		except Exception as e:
-			raise forms.ValidationError("Invalid URL entered", e)
-		return url
