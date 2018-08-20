4b823bc21f8124571346f47a94f2d345caa61255
alexHooper <alexmohack@gmail.com>
2018-08-20 08:45:41 +0530

    add hosts for redirection from www.

--- kirr/hosts.py
+++ kirr/hosts.py
@@ -3,5 +3,5 @@ from django_hosts import patterns, host
 
 host_patterns = patterns('',
     host(r'www', settings.ROOT_URLCONF, name='www'),
-    # host(r'(\w+)', 'path.to.custom_urls', name='wildcard'),
+    host(r'(?!www).*', 'kirr.hostsconf.urls', name='wildcard'),
 )
