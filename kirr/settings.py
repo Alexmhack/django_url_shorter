01b569094452dbbe01e872fe0d10caa71b4c51f6
alexHooper <alexmohack@gmail.com>
2018-08-20 10:06:57 +0530

    add root templates folder path for project

--- kirr/settings.py
+++ kirr/settings.py
@@ -61,7 +61,7 @@ DEFAULT_REDIRECT_URL = "http://www.djgo.com:8000"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
