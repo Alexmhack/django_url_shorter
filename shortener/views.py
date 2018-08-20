2ad5acea5f824eab190f5d608c19657ade2e279a
alexHooper <alexmohack@gmail.com>
2018-08-20 10:03:03 +0530

    add path to our template

--- shortener/views.py
+++ shortener/views.py
@@ -6,7 +6,7 @@ from .models import KirrURL
 
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
-		return render(request, 'home.html', {})
+		return render(request, 'shortener/home.html', {})
 
 
 class KirrRedirectView(View):
