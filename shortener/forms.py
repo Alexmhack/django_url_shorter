525737d058378b18f8ffe5cc4ecdba5b506fd5d0
alexHooper <alexmohack@gmail.com>
2018-08-20 16:33:32 +0530

    define methods for form and field validation

--- shortener/forms.py
+++ shortener/forms.py
@@ -2,3 +2,13 @@ from django import forms
 
 class ShortenURLForm(forms.Form):
 	url = forms.URLField()
+
+	def clean(self):
+		cleaned_data = super().clean()
+		url = cleaned_data['url']
+		print(url)
+
+	def cleaned_url(self):
+		url = self.cleaned_data['url']
+		print(url)
+		return url
