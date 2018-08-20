9c93e2ac5f55874adf9eff66b7a297eb17775c3f
alexHooper <alexmohack@gmail.com>
2018-08-20 17:22:45 +0530

    import validator from file and add use it

--- shortener/forms.py
+++ shortener/forms.py
@@ -2,11 +2,7 @@ from django import forms
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 
-def validate_com_url(value):
-	if 'com' not in value:
-		raise ValidationError("Entered URL is not a valid .com url")
-	return value
-
+from .validators import validate_com_url
 
 class ShortenURLForm(forms.Form):
 	url = forms.URLField(validators=[validate_com_url])
