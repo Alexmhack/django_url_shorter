423ec367c69fc7490df9a446ad9ac42c22743fc7
alexHooper <alexmohack@gmail.com>
2018-08-20 17:08:04 +0530

    remove the unused validate_url validator

--- shortener/forms.py
+++ shortener/forms.py
@@ -2,14 +2,5 @@ from django import forms
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 
-def validate_url(value):
-	url_validator = URLValidator()
-	try:
-		url_validator(value)
-	except Exception as e:
-		raise ValidationError("Enter a valid url")
-	return value
-
-
 class ShortenURLForm(forms.Form):
 	url = forms.URLField()
