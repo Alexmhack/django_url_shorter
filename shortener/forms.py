113c9d3b2c3b2005ab63f8c7dff907a962cf7c74
alexHooper <alexmohack@gmail.com>
2018-08-20 16:39:42 +0530

    use url validator and raise error

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,14 +1,18 @@
 from django import forms
+from django.core.validators import URLValidator
 
 class ShortenURLForm(forms.Form):
 	url = forms.URLField()
 
 	def clean(self):
 		cleaned_data = super().clean()
-		url = cleaned_data['url']
-		print(url)
 
 	def cleaned_url(self):
 		url = self.cleaned_data['url']
 		print(url)
+		url_validator = URLValidator()
+		try:
+			url_validator(url)
+		except Exception as e:
+			raise forms.ValidationError("Invalid URL entered", e)
 		return url
