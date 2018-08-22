d2083f93342429b21bdc4fa7a0f8b782142e70e2
alexHooper <alexmohack@gmail.com>
2018-08-23 00:34:40 +0530

    rename our model and correct our logic for creating object

--- analytics/models.py
+++ analytics/models.py
@@ -2,24 +2,29 @@ from django.db import models
 
 from shortener.models import KirrURL
 
-class ClickAnalysisManager(models.Manager):
+class ClickAnalyticManager(models.Manager):
 	def click_analyse(self, instance):
 		print(f'INSTANCE: {instance}')
 		if isinstance(instance, KirrURL):
 			obj, created = self.get_or_create(kirr_url=instance)
+			if created:
+				created.count += 1
+				created.save()
+				return obj.count
 			obj.count += 1
 			obj.save()
 			return obj.count
+				
 		return None
 
 
-class ClickAnalysis(models.Model):
+class ClickAnalytic(models.Model):
 	kirr_url = models.OneToOneField(KirrURL, on_delete=models.CASCADE)
 	count = models.IntegerField(default=0)
 	updated = models.DateTimeField(auto_now=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
-		return f"{count}"
+		return f"{self.count}"
 
-	objects = ClickAnalysisManager()
+	objects = ClickAnalyticManager()
