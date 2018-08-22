340f2c69e162070c2ebf4a4a4c6a9ff1b2112d77
alexHooper <alexmohack@gmail.com>
2018-08-22 21:42:37 +0530

    create model for storing clicks for each url

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from shortener.models import KirrURL
+
+class ClickAnalysis(models.Model):
+	kirr_url = models.OneToOneField(KirrURL)
+	count = models.IntegerField(default=0)
+	updated = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{count}"
