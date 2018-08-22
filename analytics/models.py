bc9c803c379754d57da3dbf891a2b25a97b4480d
alexHooper <alexmohack@gmail.com>
2018-08-22 21:43:19 +0530

    create model manager for click model

--- analytics/models.py
+++ analytics/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 from shortener.models import KirrURL
 
+class ClickAnalysisManager(models.Manager):
+	def click_analyse(self, instance):
+		if isinstance(KirrURL, instance):
+			obj, created = self.objects.get_or_create(kirr_url=instance)
+			obj.count += 1
+			obj.save()
+			return obj.count
+		return None
+
+
 class ClickAnalysis(models.Model):
 	kirr_url = models.OneToOneField(KirrURL)
 	count = models.IntegerField(default=0)
@@ -10,3 +20,5 @@ class ClickAnalysis(models.Model):
 
 	def __str__(self):
 		return f"{count}"
+
+	objects = ClickAnalysisManager()
