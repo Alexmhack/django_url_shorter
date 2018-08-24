73b468606c5f230cb461dd3ba44b5ce1856cef3e
alexHooper <alexmohack@gmail.com>
2018-08-24 20:17:37 +0530

    add https method

--- kirr/hostsconf/views.py
+++ kirr/hostsconf/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.http import HttpResponseRedirect
 
-DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "http://www.blooming-journey-56305.herokuapp.com")
+DEFAULT_REDIRECT_URL = getattr(settings, "DEFAULT_REDIRECT_URL", "https://www.blooming-journey-56305.herokuapp.com")
 
 def wildcard_redirect(request, path=None):
 	new_url = DEFAULT_REDIRECT_URL
